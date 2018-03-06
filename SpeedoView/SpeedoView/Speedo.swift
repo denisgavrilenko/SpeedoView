b00c5db447df6db2cf372aa3669db77c557376ed
Denis Gavrilenko <dhaurylenka@exadel.com>
2018-03-06 11:52:28 +0300

    [Colors] Update color literals

--- SpeedoView/SpeedoView/Speedo.swift
+++ SpeedoView/SpeedoView/Speedo.swift
@@ -19,11 +19,9 @@ public class Speedo: UIView {
         super.layoutSubviews()
         
         guard lastFrame != self.frame else { return }
-        
         layer.sublayers?.forEach { $0.removeFromSuperlayer() }
-        
         backgroundLayer = CAShapeLayer(bounds, color: .gray, startAngle: 30, finishAngle: 330, width: 10)
-        gradientLayer = CALayer(bounds, gradientColors: [.purple, .blue, .green, .purple])
+        gradientLayer = CALayer(bounds, gradientColors: [#colorLiteral(red: 0.009523575194, green: 0.8102305532, blue: 0.4242953062, alpha: 1), #colorLiteral(red: 0.009523575194, green: 0.8102305532, blue: 0.4242953062, alpha: 1), #colorLiteral(red: 0.810185492, green: 0.9170953631, blue: 0.1986451447, alpha: 1), #colorLiteral(red: 0.428086102, green: 0.7447001338, blue: 0.5761241317, alpha: 1), #colorLiteral(red: 0.002692978596, green: 0.5805307627, blue: 0.9376723766, alpha: 1), #colorLiteral(red: 0.002692978596, green: 0.5805307627, blue: 0.9376723766, alpha: 1), #colorLiteral(red: 0.009523575194, green: 0.8102305532, blue: 0.4242953062, alpha: 1)])
         speedoLine = SpeedoLine(bounds, color: .gray, startAngle: 30, finishAngle: 330, width: 10)
         gradientLayer.withMask(speedoLine.layer)
         layer.addSublayer(backgroundLayer)
